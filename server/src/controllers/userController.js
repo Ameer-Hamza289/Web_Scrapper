0dee8356043437aa081329b306c76475019e9bbb
Subhan <muhammadsubhan8934@gmail.com>
2024-08-18 10:55:42 +0500

    bacjend development

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,7 +1,80 @@
-// controllers/userController.js
-const User = require('../models/User');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
+// // controllers/userController.js
+// const User = require('../models/User');
+// const jwt = require('jsonwebtoken');
+// const bcrypt = require('bcryptjs');
+
+// // Register a new user
+// exports.registerUser = async (req, res) => {
+//     const { username, email, password } = req.body;
+
+//     try {
+//         let user = await User.findOne({ email });
+//         if (user) {
+//             return res.status(400).json({ message: 'User already exists' });
+//         }
+
+//         user = new User({
+//             username,
+//             email,
+//             password
+//         });
+
+//         await user.save();
+
+//         const token = jwt.sign({ id: user._id }, 'your_jwt_secret', {
+//             expiresIn: '1h'
+//         });
+
+//         res.status(201).json({ token });
+//     } catch (error) {
+//         res.status(500).json({ message: 'Server error' });
+//     }
+// };
+
+// // Login a user
+// exports.loginUser = async (req, res) => {
+//     const { email, password } = req.body;
+
+//     try {
+//         const user = await User.findOne({ email });
+//         if (!user) {
+//             return res.status(400).json({ message: 'Invalid credentials' });
+//         }
+
+//         const isMatch = await bcrypt.compare(password, user.password);
+//         if (!isMatch) {
+//             return res.status(400).json({ message: 'Invalid credentials' });
+//         }
+
+//         const token = jwt.sign({ id: user._id }, 'your_jwt_secret', {
+//             expiresIn: '1h'
+//         });
+
+//         res.json({ token });
+//     } catch (error) {
+//         res.status(500).json({ message: 'Server error' });
+//     }
+// };
+
+// // Get user details
+// exports.getUserProfile = async (req, res) => {
+//     try {
+//         const token = req.header('Authorization').replace('Bearer ', '');
+//         const decoded = jwt.verify(token, 'your_jwt_secret');
+//         const user = await User.findById(decoded.id).select('-password');
+        
+//         if (!user) {
+//             return res.status(404).json({ message: 'User not found' });
+//         }
+
+//         res.json(user);
+//     } catch (error) {
+//         res.status(500).json({ message: 'Server error' });
+//     }
+// };
+
+
+const jwtSecret = process.env.JWT_SECRET;
 
 // Register a new user
 exports.registerUser = async (req, res) => {
@@ -21,7 +94,7 @@ exports.registerUser = async (req, res) => {
 
         await user.save();
 
-        const token = jwt.sign({ id: user._id }, 'your_jwt_secret', {
+        const token = jwt.sign({ id: user._id }, jwtSecret, {
             expiresIn: '1h'
         });
 
@@ -46,7 +119,7 @@ exports.loginUser = async (req, res) => {
             return res.status(400).json({ message: 'Invalid credentials' });
         }
 
-        const token = jwt.sign({ id: user._id }, 'your_jwt_secret', {
+        const token = jwt.sign({ id: user._id }, jwtSecret, {
             expiresIn: '1h'
         });
 
@@ -60,7 +133,7 @@ exports.loginUser = async (req, res) => {
 exports.getUserProfile = async (req, res) => {
     try {
         const token = req.header('Authorization').replace('Bearer ', '');
-        const decoded = jwt.verify(token, 'your_jwt_secret');
+        const decoded = jwt.verify(token, jwtSecret);
         const user = await User.findById(decoded.id).select('-password');
         
         if (!user) {
